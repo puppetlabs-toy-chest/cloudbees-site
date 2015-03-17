08a76d195abedf2b18575c63475696e8f770bf52
Carl Caum <carl@carlcaum.com>
2015-03-16 19:21:37 -0700

    Adding app spec tests

---
+++ spec/acceptance/wordpress_app_spec.rb
@@ -0,0 +1,13 @@
+describe 'wordpress application' do
+  it 'should respond with 302 redirect to web based installer' do
+    ip = ENV['WORDPRESS_HOST']
+    url = "http://#{ip}"
+
+    uri = URI.parse(url)
+    http = Net::HTTP.new( uri.host, uri.port)
+    response = http.request(Net::HTTP::Get.new(uri.request_uri))
+  
+    expect(response.code).to eq("302")
+    expect(response['location']).to eq("#{url}/wp-admin/install.php")
+  end
+end
