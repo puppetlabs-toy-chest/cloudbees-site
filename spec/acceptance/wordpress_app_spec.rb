dc20e3d847ebcbf00ed3579a61e919124778481a
Carl Caum <carl@carlcaum.com>
2015-04-14 16:29:09 -0700

    Simplify acceptance tests to only test application

--- spec/acceptance/wordpress_app_spec.rb
+++ spec/acceptance/wordpress_app_spec.rb
@@ -1,28 +1,15 @@
-require 'spec_helper_acceptance'
+require 'net/http'
+require 'net/https'
 
-describe 'profile::wordpress::integrated' do
-  it 'should apply' do
-    pp = <<-EOS
-      class { 'profile::wordpress::integrated':
-        db_user     => 'wordpress',
-        db_password => 'wordpress',
-      }
-    EOS
+describe 'wordpress application' do
+  it 'should respond with 302 redirect to web based installer' do
+    url = ENV['ACCEPTANCE_WORDPRESS_URL']
 
-    apply_manifest(pp, :catch_failures => true)
-  end
-
-  describe 'wordpress application' do
-    it 'should respond with 302 redirect to web based installer' do
-      ip = fact('ipaddress')
-      url = "http://#{ip}"
-
-      uri = URI.parse(url)
-      http = Net::HTTP.new( uri.host, uri.port)
-      response = http.request(Net::HTTP::Get.new(uri.request_uri))
-    
-      expect(response.code).to eq("302")
-      expect(response['location']).to eq("#{url}/wp-admin/install.php")
-    end
+    uri = URI.parse(url)
+    http = Net::HTTP.new( uri.host, uri.port)
+    response = http.request(Net::HTTP::Get.new(uri.request_uri))
+  
+    expect(response.code).to eq("302")
+    expect(response['location']).to eq("#{url}/wp-admin/install.php")
   end
 end
